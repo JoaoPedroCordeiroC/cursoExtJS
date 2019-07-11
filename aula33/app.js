c4055b7bfbf24d157460dd680ad70f4a26015602
unknown <joao.u@hotmail.com>
2019-07-11 17:06:36 -0300

    Exemplo de Anchor Layout

---
+++ aula33/app.js
@@ -0,0 +1,42 @@
+Ext.define('Curso.Janela', {
+    extend: 'Ext.window.Window',
+
+    height: 400,
+    width:500,
+    title: 'Aula 32  - Anchor Layout',
+
+    autoShow: true,
+
+    /* Ativa a barra de scroll, pois se não ativada não aparece os campos que 
+    ficaram fora da area */
+    autoScroll: true,
+
+    // Definimos que o layout é o anchor
+    layout: 'anchor',
+
+    items: [
+        {
+            xtype: 'panel',
+            title: 'painel 1',
+            // Passamos primeiro a largura e depois a altura
+            anchor: '99%, 25%'
+        },
+        {
+            xtype: 'panel',
+            title: 'painel 2',
+            anchor: '90%, 50%'     
+        },
+        {
+            xtype: 'panel',
+            title: 'painel 3', 
+            anchor: '50%, 20%'       
+        }
+        
+    ]
+});
+
+Ext.onReady(function() {
+
+    Ext.create('Curso.Janela');
+    
+});
