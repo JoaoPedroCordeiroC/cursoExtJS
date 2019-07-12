a00d4bbac224efe5b7efd5342d2d88c6a91b0eb6
unknown <joao.u@hotmail.com>
2019-07-12 13:56:03 -0300

    Exemplo VBox Layout

--- aula41/app.js
+++ aula41/app.js
@@ -7,16 +7,18 @@ Ext.define('Curso.Janela', {
     title: 'Aula 41  - VBox Layout',
     autoShow: true,
     layout: {
-        type: 'vbox',
+        type: 'vbox'//,
         // Stretch pegará toda a altura do container pai para todos os items panels
         // O stretchmax pegará a altura do panel maior e jogar para os outros 
-        align: 'stretchmax'
+        // Podemos usar também o left/center/right
+        //align: 'stretch'
     },
     items: [
         {
             xtype: 'panel',
             title: 'Painel 1',
             html: 'VBox Layout - tela 50%',
+            width: 200,
             /* O flex soma o tamanho de todos os flex e divide o tamanho do panel
                 para eles. Cada flex terá a procentagem da soma definida
                 em cada um deles. */ 
@@ -31,7 +33,7 @@ Ext.define('Curso.Janela', {
         {
             xtype: 'panel',
             title: 'Painel 3',
-            html: 'VBox Layout - tela 50%',
+            html: 'VBox Layout - tela 50% -------------------',
             dockedItems: [{
                 xtype: 'toolbar',
                 dock: 'bottom',
@@ -39,6 +41,7 @@ Ext.define('Curso.Janela', {
                     text: 'Next'
                 }]
             }],
+            width: '50%',
             flex: 3 // 3/6
         }
 
