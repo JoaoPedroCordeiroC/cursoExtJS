c663b4ad010ee5fb2740d2c0ee4312b20694fa88
unknown <joao.u@hotmail.com>
2019-07-12 10:57:26 -0300

    Exemplo Column Layout

---
+++ aula39/app.js
@@ -0,0 +1,38 @@
+Ext.define('Curso.Janela', {
+    extend: 'Ext.window.Window',
+
+    height: 500,
+    width: 700,
+
+    title: 'Aula 39  - Column Layout',
+    autoShow: true,
+
+    //autoScroll: true,
+
+    // Definimos o tipo do layout
+    layout: 'column',
+    items: [
+        {
+            xtype: 'panel',
+            title: 'Painel 1',
+            html: 'Column Layout - tela 50%',
+            width: 200
+        },
+        {
+            xtype: 'panel',
+            title: 'Painel 2',
+            html: 'Column Layout - tela 50%',
+            /* columnWidth define a largura com porcentagem. Como no panel 1 
+            definimos a largura em pixels, a columnWidth usará a porcentagem 
+            do espaço restante */
+            columnWidth: 1
+        }
+
+    ]
+});
+
+Ext.onReady(function() {
+
+    Ext.create('Curso.Janela');
+    
+});
